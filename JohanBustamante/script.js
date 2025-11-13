0a0ccf370e5ea6aa23fb7674607cd4d5769f5c86
Aleho16 <perez.alejo.rojas@gmail.com>
2025-11-13 11:44:13 -0500

    Feat Se hace el primer ejercicio del taller-clase

---
+++ JohanBustamante/script.js
@@ -0,0 +1,15 @@
+//Necesito un programa que calcule el total de mi compra en el supermercado. 
+// Pide el precio de tres productos diferentes y muestra el total a pagar.
+console.log("Sus productos y su precio son: ")
+let precioUno = 2000
+let precioDos = 5000
+let precioTres = 4000
+
+console.log("Leche $ " , precioTres)
+console.log("Huevos $ " , precioDos)
+console.log("Azucar $ " , precioUno)
+
+let totalCompra = (precioUno  + precioDos  + precioTres)
+
+
+console.log("Total a pagar: " , totalCompra)
