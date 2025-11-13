fd20f87596685823c46a7077a9fce3fa95db4077
Aleho16 <perez.alejo.rojas@gmail.com>
2025-11-13 12:14:56 -0500

    Feat Se hace el tercer ejercicio del taller-clase

--- JohanBustamante/script.js
+++ JohanBustamante/script.js
@@ -1,4 +1,4 @@
-//Necesito un programa que calcule el total de mi compra en el supermercado. 
+//1. Necesito un programa que calcule el total de mi compra en el supermercado. 
 // Pide el precio de tres productos diferentes y muestra el total a pagar.
 
 //prompt("Ingrese sus productos: ")
@@ -17,7 +17,7 @@ let totalCompra = (precioUno  + precioDos  + precioTres)
 
 console.log("Total a pagar: " , totalCompra)
 
-//Para un evento, necesito un banner. Pide el ancho y el alto de una pared (en metros) y calcula su área para saber cuánto material comprar.
+//2. Para un evento, necesito un banner. Pide el ancho y el alto de una pared (en metros) y calcula su área para saber cuánto material comprar.
 
 //prompt("Ingrese el ancho de la pared en metros: ")
 
@@ -32,6 +32,14 @@ console.log("La altura de la pared en metros es: " , alturaPared)
 console.log("El area de la pared en metros cuadrados es: " , areaPared)
 
 
+//3. Debo calcular cuánto perímetro tiene un potrero rectangular. Pide el largo y el ancho del potrero y muestra el resultado.
+let largoPotrero = prompt("Ingrese el largo del potrero ")
+let anchoPotrero = prompt("Ingrese el ancho del potrero ")
+let perimetroPotrero = 2 * (largoPotrero+anchoPotrero)
+
+console.log("El largo del potrero es: " , largoPotrero)
+console.log("La ancho  del potrero es: " , anchoPotrero)
+console.log("El perimetro del potrero es: " , perimetroPotrero )
 
 
 
